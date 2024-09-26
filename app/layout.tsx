38bf3ee9b4d97a7b9e4c8560b8495d9467c2243c
Ubuntu <stevan@ubuntu.neo.internal>
2024-09-26 22:31:26 +0700

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Welcome to stevan.io",
+  description: "Literally stevan in artifical form",
   appleWebApp: {
-    title: "NextChat",
+    title: "stevan.io",
     statusBarStyle: "default",
   },
 };
